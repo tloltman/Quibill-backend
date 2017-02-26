b0cd88771635ed9ccc81acefab001afdcf98cef2
toltman <tzaddik.oltman@gmail.com>
2017-02-25 21:39:50 -0600

    Require authorization for admin area

--- src/Quibill.Web/Areas/Admin/Controllers/HomeController.cs
+++ src/Quibill.Web/Areas/Admin/Controllers/HomeController.cs
@@ -6,6 +6,7 @@ using System.Web.Mvc;
 
 namespace Quibill.Web.Areas.Admin.Controllers
 {
+    [Authorize(Roles = "Admin")]
     public class HomeController : Controller
     {
         // GET: Admin/Home
