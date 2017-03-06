0e77512897ea609bd8f1d17b19b4212b2fd17856
toltmann <toltman@saturnsys.com>
2017-03-06 12:03:30 -0600

    #5 Finished login functionality

--- src/Quibill.Web/Scripts/login.js
+++ src/Quibill.Web/Scripts/login.js
@@ -3,11 +3,36 @@
 
     var tokenKey = 'accessToken';
 
+    function appendError(errorString) {
+        $('#errors-container').show();
+        $('#errors-container ul').append("<li>" + errorString + "</li>");
+    }
+
+    function clearErrors() {
+        $('#errors-container ul').empty();
+    }
+
     function showError(jqXHR) {
-
-        console.log(jqXHR.status + ': ' + jqXHR.statusText);
-
-        console.log(jqXHR.responseJSON);
+        clearErrors();
+        var response = jqXHR.responseJSON;
+        if (response) {
+            if (response.error_description) {
+                appendError(response.error_description);
+            }
+            if (response.ModelState) {
+                var modelState = response.ModelState;
+                for (var errorProp in modelState) {
+                    if (modelState.hasOwnProperty(errorProp)) {
+                        var errorArray = modelState[errorProp];
+                        if (errorArray.length) {
+                            for (var i = 0; i < errorArray.length; ++i) {
+                                appendError(errorArray[i]);
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 
     self.getValues = function () {
@@ -17,12 +42,13 @@
             headers.Authorization = 'Bearer ' + token;
         }
 
+
         $.ajax({
             type: 'GET',
             url: '/api/values/1',
             headers: headers
         }).done(function (data) {
-            alert("Got value: " + data);
+            $('#values').append("<li>" + data + "</li>");
         }).fail(showError);
     }
 
@@ -56,15 +82,34 @@
             contentType: 'application/x-www-form-urlencoded; charset=utf-8',
             data: loginData
         }).done(function(data) {
-            alert("User: " + data.userName + " successfully logged in!");
+            window.location.replace('/Home/Index');
             sessionStorage.setItem(tokenKey, data.access_token);
         }).fail(showError);
     };
 
-    self.logout = function() {
-        sessionStorage.removeItem(tokenKey);
-        alert("Successfully logged out!")
+    self.logout = function () {
+        var token = sessionStorage.getItem(tokenKey);
+        var headers = {};
+        if (token) {
+            headers.Authorization = 'Bearer ' + token;
+        }
+
+        $.ajax({
+            type: 'POST',
+            url: '/api/Account/Logout',
+            headers: headers
+        }).done(function (data) {
+            window.location.replace('/Home/Index');
+            sessionStorage.removeItem(tokenKey);
+        }).fail(showError);
     }
 }
 
-var loginPage = new LoginPage();
+$(function() {
+    var loginPage = new LoginPage();
+
+    $('#registerButton').click(loginPage.register);
+    $('#loginButton').click(loginPage.login);
+    $('#logoutButton').click(loginPage.logout);
+    $('#getValuesButton').click(loginPage.getValues);
+});
